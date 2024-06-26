2b0c3ef8c6c87fb91f24431ea4edc6986ca17490
unmeshup <unplfc@gmail.com>
2024-06-26 16:21:15 +0530

    Vercel routing issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import ProjectDisplay from "./pages/ProjectDisplay";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
-          <Route path="/experience" element={<Experience />} />
-        </Routes>
+       
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/project/:id" element={<ProjectDisplay />} />
+            <Route path="/experience" element={<Experience />} />
+          </Routes>
+        
         <Footer />
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
